d8f4eec317ce5962ca4d9b865b8592b40f8a3626
Aleksa <zabumaphu@gmail.com>
2026-05-29 16:16:07 -0400

    data loaders

---
+++ minst_classifier/data.py
@@ -0,0 +1,13 @@
+from torchvision import datasets, transforms
+from torch.utils.data import dataloader
+
+def get_loaders(batch_size=64):
+    transform = transforms.Compose([
+        transforms.ToTensor(),  # image -> [0,1] float tensor
+        transforms.Normalize((0.5,), (0.5,)) # center around 0
+    ])
+    
+    train = datasets.MNIST('.', train=True, download=True, transform=transform)
+    test = datasets.MINST('.', train=False, download=True, transform=transform)
+
+    return dataloader(train, batchsize=batch_size), dataloader(test, batch_size=batch_size)
