cdb0266d6c50f82b348407db528e795e8666e61b
Aleksa <zabumaphu@gmail.com>
2026-05-29 09:24:25 -0400

    Training function created

--- main.py
+++ main.py
@@ -36,3 +36,17 @@ class MyNeuralNet(nn.Module):
         x = self.R(self.Matrix(x))
         x = self.Matrix2(x)
         return x.squeeze()
+
+def train_model(x,y,f,n_epochs=50):
+    opt = optim.SDG(f.parameters(), lr=0.001)
+    L = nn.MSELoss()
+
+    #Train model
+    losses = []
+    for _ in range(n_epochs):
+        optim.zero_grad() # Flush previous eopch's gradient
+        loss_value = L(f(x), y)
+        loss_value.backward() # Compute gradient
+        optim.step()
+        losses.append(loss_value.item())
+    return f, losses
